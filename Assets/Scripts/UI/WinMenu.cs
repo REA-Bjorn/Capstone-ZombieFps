85cc65daf700c12ffe9a695144e7d2241f0a31e4
Bjorn2021 <REAcosta1@student.fullsail.edu>
2024-03-14 22:15:39 -0400

    Fixed Win Menu

--- Assets/Scripts/UI/WinMenu.cs
+++ Assets/Scripts/UI/WinMenu.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class WinMenu : MonoBehaviour
 {
